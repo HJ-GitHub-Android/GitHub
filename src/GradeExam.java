848ba986355159c29e77d6c3620b95e3c50dd77e
ywlaker <2909103144@qq.com>
2020-06-30 11:39:54 +0800

    HW

---
+++ src/GradeExam.java
@@ -0,0 +1,23 @@
+public class GradeExam {
+    public static void main(String[] args) {
+        char[][] answers = {
+                {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
+                {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'E'},
+                {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
+                {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}};
+        char[] keys = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+        for (int i = 0; i < answers.length; i++) {
+            int correctCount = 0;
+            int wrongCount = 0;
+            for (int j = 0; j < answers[i].length; j++) {
+                if (answers[i][j] == keys[j]) {
+                    correctCount++;
+                } else {
+                    wrongCount++;
+                }
+            }
+            System.out.println("student:" + i + "right:" + correctCount + " false:" + wrongCount);
+        }
+    }
+}
